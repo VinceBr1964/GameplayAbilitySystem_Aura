670035d81de044983b8c59bef87e3910c9152926
VinceBr1964 <vincebr@aol.com>
2024-11-11 10:52:07 -0700

    Aura Player Controller

---
+++ Source/Aura/Private/PLayer/AuraPlayerController.cpp
@@ -0,0 +1,31 @@
+// Copyright Vince Bracken
+
+
+#include "PLayer/AuraPlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
+#include "D:/EpicGames/UE_5.1/Engine/Plugins/EnhancedInput/Source/EnhancedInput/Public/EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraContext);
+
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraContext, 0);
+
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+
+}
