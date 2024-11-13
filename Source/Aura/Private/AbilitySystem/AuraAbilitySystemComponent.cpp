0f4c2cfe67025016edfebfe0b2fde95e3373dc6d
VinceBr1964 <vincebr@aol.com>
2024-11-13 12:40:26 -0700

    S3-19 - Ability System Component and Attribute Set

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -0,0 +1,5 @@
+// Copyright Vince Bracken
+
+
+#include "AbilitySystem/AuraAbilitySystemComponent.h"
+
---
+++ Source/Aura/Private/AbilitySystem/AuraAttributeSet.cpp
@@ -0,0 +1,5 @@
+// Copyright Vince Bracken
+
+
+#include "AbilitySystem/AuraAttributeSet.h"
+
