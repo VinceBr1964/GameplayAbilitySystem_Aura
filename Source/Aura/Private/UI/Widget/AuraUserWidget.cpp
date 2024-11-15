7e2756745f02405f60f0bbb3f50fe41f8d29be20
VinceBr1964 <vincebr@aol.com>
2024-11-15 09:49:43 -0700

    S5-L29 Aura User Widget and Widget Controller

---
+++ Source/Aura/Private/UI/Widget/AuraUserWidget.cpp
@@ -0,0 +1,12 @@
+// Copyright Vince Bracken
+
+
+#include "UI/Widget/AuraUserWidget.h"
+
+void UAuraUserWidget::SetWidgetController(UObject* InWidgetController)
+{
+	WidgetController = InWidgetController;
+	WidgetControllerSet();
+
+
+}
---
+++ Source/Aura/Private/UI/WidgetController/AuraWidgetController.cpp
@@ -0,0 +1,5 @@
+// Copyright Vince Bracken
+
+
+#include "UI/WidgetController/AuraWidgetController.h"
+
