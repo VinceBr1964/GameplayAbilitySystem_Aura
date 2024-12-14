313d0faf6db29de333a98c7152fc20df7a6f4e27
VinceBr1964 <vincebr@aol.com>
2024-12-13 17:54:21 -0700

    S12-L128: Character Class Info

---
+++ Source/Aura/Private/AbilitySystem/data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Vince Bracken
+
+
+#include "AbilitySystem/data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
