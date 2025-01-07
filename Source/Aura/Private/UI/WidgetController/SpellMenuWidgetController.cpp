3528819f84b7186389cc520fbd2a7e85b50569e7
VinceBr1964 <vincebr@aol.com>
2025-01-07 09:14:40 -0700

    S24 - L286: Show Spell Points

--- Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -4,10 +4,13 @@
 #include "UI/WidgetController/SpellMenuWidgetController.h"
 #include "AbilitySystem/AuraAbilitySystemComponent.h"
 #include "AbilitySystem/data/AbilityInfo.h"
+#include "PLayer/AuraPlayerState.h"
 
 void USpellMenuWidgetController::BroadcastInitialValues()
 {
 	BroadcastAbilityInfo();
+	SpellPointsChanged.Broadcast(GetAuraPS()->GetSpellPoints());
+
 }
 
 void USpellMenuWidgetController::BindCallbacksToDependencies()
@@ -20,5 +23,10 @@ void USpellMenuWidgetController::BindCallbacksToDependencies()
 				Info.StatusTag = StatusTag;
 				AbilityInfoDelegate.Broadcast(Info);
 			}
+		});
+
+	GetAuraPS()->OnSpellPointsChangedDelegate.AddLambda([this](int32 SpellPoints)
+		{
+			SpellPointsChanged.Broadcast(SpellPoints);
 		});
 }
