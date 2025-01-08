8ab1a559a46e5d10f05362a40cfdfcba9bc6d639
VinceBr1964 <vincebr@aol.com>
2025-01-08 10:27:01 -0700

    S24 - L294: Firebolt Descriptions

--- Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraProjectileSpell.h
@@ -16,6 +16,10 @@ UCLASS()
 class AURA_API UAuraProjectileSpell : public UAuraDamageGameplayAbility
 {
 	GENERATED_BODY()
+public:
+	virtual FString GetDescription(int32 Level) override;
+	virtual FString GetNextLevelDescription(int32 Level) override;
+
 
 protected:
 
@@ -26,6 +30,9 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly)
 	TSubclassOf<AAuraProjectile> ProjectileClass;
+
+	UPROPERTY(EditDefaultsOnly)
+	int32 NumProjectiles = 5;
 
 
 };
