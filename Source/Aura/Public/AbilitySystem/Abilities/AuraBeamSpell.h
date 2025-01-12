f4b22b0ef422557d24837d002e7022daf2e1ef6c
VinceBr1964 <vincebr@aol.com>
2025-01-11 18:54:36 -0700

    S26 - L321: Aura Beam Spell

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -0,0 +1,35 @@
+// Copyright Vince Bracken
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystem/Abilities/AuraDamageGameplayAbility.h"
+#include "AuraBeamSpell.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraBeamSpell : public UAuraDamageGameplayAbility
+{
+	GENERATED_BODY()
+public:
+	
+	UFUNCTION(BlueprintCallable)
+	void StoreMouseDataInfo(const FHitResult& HitResult);
+
+	UFUNCTION(BlueprintCallable)
+	void StoreOwnerPlayerController();
+
+
+protected:
+
+	UPROPERTY(BlueprintreadWrite, Category = "Beam")
+	FVector MouseHitLocation;
+
+	UPROPERTY(BlueprintreadWrite, Category = "Beam")
+	TObjectPtr<AActor> MouseHitActor;
+	
+	UPROPERTY(BlueprintreadWrite, Category = "Beam")
+	TObjectPtr<APlayerController> OwnerPlayerController;
+};
