1e45f9bea0d7ea3a02b782a7b864eddb4cc61190
VinceBr1964 <vincebr@aol.com>
2025-01-12 16:25:44 -0700

    S26 - L327: Target Trace Channel

--- Source/Aura/Aura.h
+++ Source/Aura/Aura.h
@@ -4,5 +4,7 @@
 
 #include "CoreMinimal.h"
 
-#define CUSTOM_DEPTH_RED 125
+#define CUSTOM_DEPTH_RED 250
 #define ECC_Projectile ECollisionChannel::ECC_GameTraceChannel1
+#define ECC_Target ECollisionChannel::ECC_GameTraceChannel2
+
