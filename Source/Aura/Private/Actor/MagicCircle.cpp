432302cebcd75ebfad6efa47f4213bfeea30e02c
VinceBr1964 <vincebr@aol.com>
2025-01-16 09:11:45 -0700

    S28-L345: Magic Circle

---
+++ Source/Aura/Private/Actor/MagicCircle.cpp
@@ -0,0 +1,32 @@
+// Copyright Vince Bracken
+
+
+#include "Actor/MagicCircle.h"
+#include "Components/DecalComponent.h"
+
+
+AMagicCircle::AMagicCircle()
+{
+ 	
+	PrimaryActorTick.bCanEverTick = true;
+
+	MagicCircleDecal = CreateDefaultSubobject<UDecalComponent>("MagicCircleDecal");
+	MagicCircleDecal->SetupAttachment(RootComponent);
+
+
+}
+
+
+void AMagicCircle::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void AMagicCircle::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
