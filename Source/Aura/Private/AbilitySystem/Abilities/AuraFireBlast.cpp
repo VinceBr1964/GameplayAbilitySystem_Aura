395516ad3a1fbd6d5e8232a70fe289d7128c003b
VinceBr1964 <vincebr@aol.com>
2025-01-21 09:45:02 -0700

    S29 - L365: Aura Fire Blast

--- Source/Aura/Private/AbilitySystem/Abilities/AuraFireBlast.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBlast.cpp
@@ -76,3 +76,8 @@ FString UAuraFireBlast::GetNextLevelDescription(int32 Level)
 		NumFireBalls,
 		ScaledDamage);
 }
+
+TArray<AAuraFireBall*> UAuraFireBlast::SpawnFireBalls()
+{
+	return TArray<AAuraFireBall*>();
+}
